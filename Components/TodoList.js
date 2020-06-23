bad12560635a8907d28423f65b960cca84b3ed01
Diogo14M <diogomartins200214@gmail.com>
2020-06-23 15:13:31 +0100

    displaying-lists

--- Components/TodoList.js
+++ Components/TodoList.js
@@ -3,11 +3,27 @@ import {StyleSheet, Text, View} from 'react-native';
 import colors from '../Colors';
 
 export default TodoList = ({list}) => {
+  const completedCount = list.todos.filter(todo => todo.completed).length;
+  const remainingCount = list.todos.length - completedCount;
+
   return (
     <View style={[styles.listContainer, {backgroundColor: list.color}]}>
       <Text style={styles.listTitle} numberOfLines={1}>
         {list.name}
       </Text>
+
+      <View>
+        <View style={{alignItems: "center"}}>
+          <Text style={styles.count}>{completedCount}</Text>
+          <Text style={styles.subtitle}>Em falta</Text>
+        </View>
+
+        <View style={{alignItems: "center"}}>
+          <Text style={styles.count}>{remainingCount}</Text>
+          <Text style={styles.subtitle}>Completo</Text>
+        </View>
+        
+      </View>
     </View>
   );
 };
@@ -26,5 +42,15 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: colors.white,
     marginBottom: 18
+  },
+  count: {
+    fontSize: 48,
+    fontWeight: "200",
+    color: colors.white
+  },
+  subtitle: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: colors.white
   }
 })
