da78b8fa508055f3f5777352e32adc7900a3985d
Diogo14M <diogomartins200214@gmail.com>
2020-06-23 22:15:54 +0100

    add list-modal and color-picker

---
+++ Components/AddListModal.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity, TextInput } from "react-native";
+import {AntDesign} from '@expo/vector-icons';
+import colors from "../Colors";
+
+export default class AddListModal extends React.Component {
+  backgroundColors = ["#1abc9c", "#3498db", "#9b59b6", "#f1c40f", "#e67e22", "#95a5a6", "#c0392b"]
+
+  state= {
+    name: "",
+    color: this.backgroundColors[0]
+  }
+
+  createTodo = () => {
+    const {name, color} = this.state
+    tempData.push({
+      name,
+      color,
+      todos: []
+    })
+
+    this.setState({name: ""})
+    this.props.closeModal()
+  }
+
+  renderColors() {
+    return this.backgroundColors.map(color => {
+      return (
+        <TouchableOpacity 
+          key={color} 
+          style={[styles.colorSelect, {backgroundColor: color}]} 
+          onPress={() => this.setState({color})} 
+        />
+      )
+    })
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <TouchableOpacity style={{position: "absolute", top: 64, right: 32}} onPress={this.props.closeModal}>
+          <AntDesign name="close" size={24} color={colors.black} />
+        </TouchableOpacity>
+
+        <View style={{alignSelf: "stretch", marginHorizontal: 32}}>
+          <Text style={styles.title}>Criar lista de tarefas</Text>
+
+          <TextInput style={styles.input} 
+            placeholder="Nome da Lista?" 
+            onChangeText={text => this.setState({name: text})} 
+          />
+
+          <View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 12}}>
+            {this.renderColors()}
+          </View>
+
+          <TouchableOpacity style={[styles.create, {backgroundColor: this.state.color}]} onPress={this.createTodo}>
+            <Text style={{color: colors.white, fontWeight: "600"}}>Criar!</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: colors.black,
+    alignSelf: "center",
+    marginBottom: 16
+  },
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.blue,
+    borderRadius: 6,
+    height: 50,
+    marginTop: 8,
+    paddingHorizontal: 16,
+    fontSize: 18
+  },
+  create: {
+    marginTop: 24,
+    height: 50,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  colorSelect: {
+    width: 30,
+    height: 30,
+    borderRadius: 4
+  }
+})
